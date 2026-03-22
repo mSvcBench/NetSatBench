85eb20ad4ef020f911fc47d103ee773aca517fc9
ShahramDadras <shahram.dadras@alumni.uniroma2.eu>
2026-03-22 19:43:05 +0000

    handover start

---
+++ test/handover/grd/start-agent-grd.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo " 🚀 Launching connection-agent.py ..."
+/usr/bin/screen -S CONAGENT -s /bin/bash -t win0 -A -d -m
+sleep 1
+screen -S CONAGENT -p win0 -X stuff $'python3 -u /app/grd/connection_agent_grd.py --handover-delay 80\n'
---
+++ test/handover/usr/start-agent-usr.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo " 🚀 Launching connection-agent.py ..."
+/usr/bin/screen -S CONAGENT -s /bin/bash -t win0 -A -d -m
+sleep 5
+screen -S CONAGENT -p win0 -X stuff $'python3 -u /app/usr/connection_agent_usr.py --handover-delay 80\n'
